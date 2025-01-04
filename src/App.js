51b813a3ea16656c581eeac10324eb8351b3302e
Szymon <149382092+Szaj111@users.noreply.github.com>
2025-01-04 10:11:33 +0100

    04_01_Home_page_done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Security from "./components/Straz";
 import Data from "./components/Data";
 import Tlo from "./components/obrazy/tlo.jpg";
 import Map_icon from "./components/obrazy/mapa.png";
+import About_icon from "./components/obrazy/mark.png";
+import Data_icon from "./components/obrazy/dane.png";
+import Guard_icon from "./components/obrazy/tower.png";
+
 function Home() {
   return (
     <div className="App">
@@ -18,20 +22,41 @@ function Home() {
       <div className="Container">
         <img className="Background" src={Tlo} alt="Tło Geoportalu" />
         <div className="ButtonContainer">
-          <Link to="/map">
-            <div className="ButtonWithIcon">
-              <button className="Button1">Mapa</button>
+          <Link to="/map" className="ButtonWithIcon">
+            <div className="IconContainer">
               <img className="Map_icon" src={Map_icon} alt="Ikona mapy" />
             </div>
+            <button className="Button1">Mapa</button>
           </Link>
 
-          <Link to="/about">
-            <button className="Button2">O projekcie</button>
-          </Link>
-          <Link to="/data">
+          <div className="ButtonContainer">
+            <Link to="/about" className="ButtonWithIcon">
+              <div className="IconContainer">
+                <img
+                  className="About_icon"
+                  src={About_icon}
+                  alt="Ikona o projekcie"
+                />
+              </div>
+              <button className="Button2">O projekcie</button>
+            </Link>
+          </div>
+
+          <Link to="/data" className="ButtonWithIcon">
+            <div className="IconContainer">
+              <img className="Data_icon" src={Data_icon} alt="Ikona danych" />
+            </div>
             <button className="Button3">O danych</button>
           </Link>
-          <Link to="/security">
+
+          <Link to="/security" className="ButtonWithIcon">
+            <div className="IconContainer">
+              <img
+                className="Guard_icon"
+                src={Guard_icon}
+                alt="Ikona straży granicznej"
+              />
+            </div>
             <button className="Button4">Straż graniczna</button>
           </Link>
         </div>
--- src/components/Straz.js
+++ src/components/Straz.js
@@ -1,7 +1,35 @@
 import React from "react";
 import "./Straz.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "./Map.css";
+import { Link } from "react-router-dom";
+
 function Straz() {
-  return <div>Straz</div>;
+  return (
+    <div className="map-container">
+      <h1 className="map-title">Mapa Geoportalu</h1>
+      <MapContainer
+        center={[52.237049, 21.017532]}
+        zoom={7}
+        style={{ height: "500px", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+      </MapContainer>
+      <Link to="/">
+        <button className="button">Powrót </button>
+      </Link>
+
+      <p className="map-description">
+        Mapa przedstawiająca rozmieszczenie Posterunków Straży Granicznej oraz
+        przejść granicznych .
+      </p>
+    </div>
+  );
 }
 
 export default Straz;

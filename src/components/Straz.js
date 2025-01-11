d351c1ebb0ddc166736fc9ada441f20436d96f64
Szymon <149382092+Szaj111@users.noreply.github.com>
2025-01-11 12:17:17 +0100

    mapa_strazy_wgranie_wfs

--- src/components/Straz.js
+++ src/components/Straz.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Straz.css";
-import { MapContainer, TileLayer } from "react-leaflet";
-
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import "./Map.css";
 import { Link } from "react-router-dom";
 
 function Straz() {
+  const [geoData, setGeoData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(
+      "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Aposterunki&maxFeatures=50&outputFormat=application%2Fjson"
+    )
+      .then((response) => response.json())
+      .then((data) => setGeoData(data));
+  }, []);
+  React.useEffect(() => {
+    fetch(
+      "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Aprzejscia_graniczne&maxFeatures=50&outputFormat=application%2Fjson"
+    )
+      .then((response) => response.json())
+      .then((data) => setGeoData(data));
+  }, []);
+
   return (
     <div className="map-container">
       <h1 className="map-title">Mapa Geoportalu</h1>
@@ -19,14 +34,15 @@ function Straz() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
+        {/* Warstwa GeoJSON */}
+        {geoData && <GeoJSON data={geoData} />}
       </MapContainer>
       <Link to="/">
         <button className="button">Powrót </button>
       </Link>
-
       <p className="map-description">
         Mapa przedstawiająca rozmieszczenie Posterunków Straży Granicznej oraz
-        przejść granicznych .
+        przejść granicznych.
       </p>
     </div>
   );

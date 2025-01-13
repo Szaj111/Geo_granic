3fe31b6a1054c2b6df3d3cc33e909b84c0210f97
Szymon <149382092+Szaj111@users.noreply.github.com>
2025-01-13 13:41:52 +0100

    About_rdy

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./About.css";
+import Tlo from "./obrazy/tlo2.jpg";
 
 function About() {
   return (
     <div className="container">
-      <div className="tytul">Opis Geoportalu</div>
-      <div className="opis">
-        Praca ma na celu identyfikację potencjalnych miejsc, w których
-        nielegalni imigranci mogą przekraczać granicę polsko-białoruską. Analiza
-        ma pomóc w wskazaniu obszarów wymagających wzmożonej ochrony oraz
-        wspomóc działania związane z zabezpieczeniem granicy.
+      <div className="overlay">
+        <div className="tytul">Opis Geoportalu</div>
+        <div className="opis">
+          Praca ma na celu identyfikację potencjalnych miejsc, w których
+          nielegalni imigranci mogą przekraczać granicę polsko-białoruską.
+          Analiza ma pomóc w wskazaniu obszarów wymagających wzmożonej ochrony
+          oraz wspomóc działania związane z zabezpieczeniem granicy.
+        </div>
       </div>
     </div>
   );
